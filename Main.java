c6ddac114f3c937a679a974f6016cf81bf804824
vladislav0330 <84127884+vladislav0330@users.noreply.github.com>
2021-05-13 13:22:38 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,23 @@
+public class Main {
+    public static void main(String[] args) {
+        String offer = "Сережа целый день играл в майнкрафт.";
+        int k = 5;
+        char symbol = '#';
+        System.out.printf("Дано: %s%sРезультат: %s", offer, System.lineSeparator(), Main(offer, k, symbol));
+    }
+ 
+    public static String Main(String offer, int index, char symbol) {
+        StringBuilder sb = new StringBuilder();
+        if (offer != null && offer.length() > 0 && index > 0) {
+            for (String word : offer.split(" ")) {
+                if (word.length() >= index) {
+                    sb.append(word.substring(0, index - 1)).append(symbol).append(word.substring(index, word.length()));
+                } else {
+                    sb.append(word);
+                }
+                sb.append(" ");
+            }
+        }
+        return sb.toString();
+    }
+}
